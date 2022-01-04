a88514d0abfa54748c1427217c1fd6cc0098e7ea
RussellLin <superbr7789@gmail.com>
2022-01-04 22:44:22 +0800

    Refactor style settings using tailwindCSS

--- src/calculator.jsx
+++ src/calculator.jsx
@@ -51,19 +51,32 @@ class Calculator extends Component {
         index: 11,
         number: ''
       }
-    ]
+    ],
+    total: 0,
+    title: 'React Calculator'
   }
 
   render() {
     return (
-      <div>
-        <h1 className='text-3xl font-bold underline'>Hello Calculator!</h1>
-        <div className="number-area" style={{ display: "grid", gridTemplateColumns: 'repeat(3, 1fr)' }}>
-          {
-            this.state.numbers.map((number) => {
-              return <div className='btn' key={number.index}>{number.number}</div>
-            })
-          }
+      <div className='outer-container'>
+        <h1 className='text-3xl font-bold mb-4 mt-4 pb-4 pt-8 text-center text-white'>{this.state.title}</h1>
+        <div className="all-container">
+          <div className="calculate-result-area total">{this.state.total}</div>
+          <div className="calculate-area">
+            <div className="plus-minus-mutiply-divide-area">
+              <div className="plus calculation">+</div>
+              <div className="minus calculation">-</div>
+              <div className="multiply calculation">x</div>
+              <div className="divide calculation">/</div>
+            </div>
+            <div className="number-area">
+              {
+                this.state.numbers.map((number) => {
+                  return <div className='btn' key={number.index}>{number.number}</div>
+                })
+              }
+            </div>
+          </div>
         </div>
       </div>
     )
