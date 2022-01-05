7f62279658de42dec266164ecabae810c5877052
RussellLin <superbr7789@gmail.com>
2022-01-05 22:09:18 +0800

    Add simple svg icon via React heroicon

--- src/calculator.jsx
+++ src/calculator.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BeakerIcon } from '@heroicons/react/solid'
 
 class Calculator extends Component {
   state = {
@@ -107,7 +108,12 @@ class Calculator extends Component {
     const { title, total, numbers, arithmetic } = this.state
     return (
       <div className='outer-container'>
-        <h1 className='title'>{title}</h1>
+        <div className="title-area">
+          <h1 className='title'>{title}</h1>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="#ffffff" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
+          </svg>
+        </div>
         <div className="all-container">
           <div className="show-area">
             <div className="total">{total}</div>
@@ -127,6 +133,9 @@ class Calculator extends Component {
                 })
               }
             </div>
+          </div>
+          <div className="picture-area">
+            <img src="https://www.freepik.com/free-vector/calculator-concept-illustration_6982904.htm#query=calculate&position=3&from_view=author" alt="" className="src" />
           </div>
         </div>
       </div>
